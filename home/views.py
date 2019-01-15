09b2b2e85ce99b0c08e343ebc6d6c979812d0524
Crowdbotics <team@crowdbotics.com>
2019-01-15 21:44:56 +0000

    add package google-assistant-sdk

--- home/views.py
+++ home/views.py
@@ -7,6 +7,8 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'google-assistant-sdk', 'url': 'http://pypi.python.org/pypi/google-assistant-sdk/0.4.2'},
+	{'name':'eth-utils', 'url': 'http://pypi.python.org/pypi/eth-utils/0.7.4'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
 	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
